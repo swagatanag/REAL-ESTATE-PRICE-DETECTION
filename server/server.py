2a0e89acc2708f6ff1ae6da36135568d8fbb82fb
swagatanag <nagdeymanika@gmail.com>
2025-09-09 21:20:14 +0530

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, request, jsonify
-import server.util as util  # ✅ import util inside server folder
+from flask import Flask, request, jsonify, render_template
+import util
 
-app = Flask(__name__, template_folder="../templates", static_folder="../static")
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 @app.route('/')
 def home():
-    return render_template("index.html")  # loads templates/index.html
+    # Renders your index.html
+    return render_template("index.html")
 
-@app.route('/get_location_names')
+@app.route('/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
         'locations': util.get_location_names()
@@ -17,21 +18,27 @@ def get_location_names():
 
 @app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
-    total_sqft = float(request.form['total_sqft'])
-    location = request.form['location']
-    bhk = int(request.form['bhk'])
-    bath = int(request.form['bath'])
+    data = request.form if request.form else request.json
+
+    total_sqft = float(data['total_sqft'])
+    location = data['location']
+    bhk = int(data['bhk'])
+    bath = int(data['bath'])
+
+    estimated_price = util.get_estimated_price(location, total_sqft, bhk, bath)
 
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+        'estimated_price': estimated_price
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+
 if __name__ == "__main__":
+    print("Starting Flask server for Real Estate Price Prediction...")
     util.load_saved_artifacts()
-    print("🏠 Real Estate Price Prediction app is running...")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
 
 
 
